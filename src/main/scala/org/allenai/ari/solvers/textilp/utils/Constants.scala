1fabc37ca027b0ed454a147ba0fe0bdeb78750c6
khashab2 <khashab2@illinois.edu>
2017-02-20 00:30:46 -0600

    redis host --> localhost.

--- src/main/scala/org/allenai/ari/solvers/textilp/utils/Constants.scala
+++ src/main/scala/org/allenai/ari/solvers/textilp/utils/Constants.scala
@@ -12,7 +12,7 @@ object Constants {
   val useRedisCachingForAnnotation = true
   val useRedisCachingForElasticSearch = false
 
-  val redisServer = "tableilp-light.dev.ai2"  //  //"tableilp16c1.dev.ai2" "localhost"
+  val redisServer = "localhost" // "tableilp-light.dev.ai2"  //  //"tableilp16c1.dev.ai2"
   val redisPort = Protocol.DEFAULT_PORT
   val timeout = 20000
 
